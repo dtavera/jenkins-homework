97e3566b07894c870f16b18bcabfd0fffe285b89
diego_tavera <diego_tavera@epam.com>
2024-10-30 12:13:01 -0600

    Initial files

---
+++ src/main/java/com/devtav/jenkinshomework/JenkinsHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.devtav.jenkinshomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devtav/jenkinshomework/JenkinsHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtav.jenkinshomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
